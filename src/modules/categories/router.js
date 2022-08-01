f2538f776babcfd2af86ce9f33bb88f78818be38
shoxizindayev <farhodshohizindayev@gmail.com>
2022-08-01 21:43:14 +0300

    hj

---
+++ src/modules/categories/router.js
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import controller from "./controller.js";
+
+const router = Router();
+
+router.get("/categories", controller.GET);
+router.post("/categories", controller.POST);
+router.put("/categories", controller.PUT);
+router.delete("/categories", controller.DELETE);
+
+export default router;
---
+++ src/modules/users/router.js
@@ -0,0 +1,11 @@
+import { Router } from "express";
+
+import controller from "./controller.js";
+import validation from "../../middlewares/validation.js";
+
+const router = Router();
+
+router.post("/login", validation, controller.LOGIN);
+router.post("/register", validation, controller.REGISTER);
+
+export default router;
